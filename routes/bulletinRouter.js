d1e3b0f9f5a958623dd05aaeca388c622edda7cc
Edith <esantiago808@gmail.com>
2020-09-12 12:59:51 -1000

    Added userid and bulletinId

--- routes/bulletinRouter.js
+++ routes/bulletinRouter.js
@@ -26,4 +26,27 @@ bulletinRouter.route('/')
     res.end('DELETE operation not supported on /bulletin');
 });
 
+bulletinRouter.route('/:bulletinId')
+.all((req, res, next) => {         
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    next();
+})
+.get((req, res) => {
+    res.end(`Will send details of the bulletin: ${req.params.bulletinId} to you`);
+})
+.post((req, res) => {
+    res.statusCode = 403;
+    res.end(`POST operations not supported on /bulletin/${req.params.bulletinId}`);
+})
+.put((req, res) => {
+    res.write(`Updating the bulletin: ${req.params.bulletinId}\n`);
+    res.end(`Will update the bulletin: ${req.body.name}
+        with description: ${req.body.description}`);
+})
+.delete((req, res) => {
+    res.statusCode = 403;
+    res.end(`DELETE operation not supported on /bulletin/${req.params.bulletinId}`);
+});
+
 module.exports = bulletinRouter;
--- routes/userRouter.js
+++ routes/userRouter.js
@@ -26,4 +26,27 @@ userRouter.route('/')
     res.end('DELETE operation not supported on /user');
 });
 
+userRouter.route('/:userId')
+.all((req, res, next) => {         
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    next();
+})
+.get((req, res) => {
+    res.end(`Will send details of the user: ${req.params.userId} to you`);
+})
+.post((req, res) => {
+    res.statusCode = 403;
+    res.end(`POST operations not supported on /user/${req.params.userId}`);
+})
+.put((req, res) => {
+    res.write(`Updating the user: ${req.params.userId}\n`);
+    res.end(`Will update the user: ${req.body.name}
+        with description: ${req.body.description}`);
+})
+.delete((req, res) => {
+    res.statusCode = 403;
+    res.end(`DELETE operation not supported on /user/${req.params.userId}`);
+});
+
 module.exports = userRouter;
